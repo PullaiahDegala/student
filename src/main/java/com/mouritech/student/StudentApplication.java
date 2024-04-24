8f63cd1d9adef5be313057461e40fbce6c2a5123
PullaiahDegala <162489108+PullaiahDegala@users.noreply.github.com>
2024-04-24 09:58:24 +0530

    change the application.java

--- src/main/java/com/mouritech/student/StudentApplication.java
+++ src/main/java/com/mouritech/student/StudentApplication.java
@@ -8,7 +8,7 @@ public class StudentApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(StudentApplication.class, args);
-		System.out.println("hiii");
+System.out.println("hello world");
 	}
 
 }
